7b757ea84138eeb1b53b2769f6d105db79cf61dd
Charles Liu <xinasuka@126.com>
2025-11-05 19:07:51 +0800

    ``` feat(license): enhance unique key generation with special characters
    Added special characters (!@#$%^&*) to the character set used for generating
    unique license keys. This change increases the entropy and security of the
    generated keys by expanding the available character pool from 62 to 70
    characters.
    ```

--- license.js
+++ license.js
@@ -1,7 +1,7 @@
 // License management utilities
 
 function generateUniqueKey() {
-  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*';
   let key = '';
   for (let i = 0; i < 16; i++) {
     key += chars.charAt(Math.floor(Math.random() * chars.length));
